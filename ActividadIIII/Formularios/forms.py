d9bee04dd1be9865e14f448e7f31292973a89046
14FO0ORTIZ <fescalanteo@miumg.edu.gt>
2021-10-06 21:43:52 -0600

    django

---
+++ ActividadIIII/Formularios/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django import forms
+from .models import Estudiante
+
+
+class EstudianteForm(forms.ModelForm):
+     class Meta:
+        model = Estudiante
+        fields = '__all__'
